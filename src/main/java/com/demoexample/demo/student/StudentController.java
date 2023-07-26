bcf4b6212192c8adf74478e617f93cf17cf62721
Mudheera Kariyawasam <2020is055@stu.ucsc.cmb.ac.lk>
2023-07-26 16:22:04 +0530

    dependency injection and properties file

--- src/main/java/com/demoexample/demo/student/StudentController.java
+++ src/main/java/com/demoexample/demo/student/StudentController.java
@@ -1,26 +1,26 @@
 package com.demoexample.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.time.LocalDate;
-import java.time.Month;
 
 @RestController
 @RequestMapping(path="api/v1/student")
 public class StudentController {
-    @GetMapping
+
+	private final StudentService studentService;
+
+	@Autowired
+	public StudentController(StudentService studentService) {
+		//this.studentService = new StudentService(); /*need to use dependency injection instead of this*/
+		this.studentService = studentService;
+	}
+
+	@GetMapping
 	public List<Student> getStudents(){
-		return List.of(
-				new Student(
-						1L,
-						"Mudheera",
-						"mudheera@gmail.com",
-						LocalDate.of(2000, Month.JULY,5),
-						21
-				)
-		);
+		return studentService.getStudents();
 	}
 }
